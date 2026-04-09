4dccf526e0243f84da5cb95043e2ebfddbea951c
phuriponga <105535944+phuriponga@users.noreply.github.com>
2026-04-09 20:42:48 +0200

    Update button.tsx

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,13 +1,16 @@
-// components/ui/button.tsx
-import * as React from "react";
+'use client'
 
-interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+import * as React from 'react'
 
-export function Button({ className, ...props }: ButtonProps) {
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  className?: string
+}
+
+export function Button({ className = '', ...props }: ButtonProps) {
   return (
     <button
-      className={`bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 ${className}`}
+      className={`bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 disabled:opacity-50 disabled:cursor-not-allowed ${className}`}
       {...props}
     />
-  );
+  )
 }
