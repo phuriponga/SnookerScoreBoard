3464e8cb11a1c7ef2038bd4c63a51764d7c125cd
phuriponga <105535944+phuriponga@users.noreply.github.com>
2026-04-10 09:58:40 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import SnookerScoreboardApp from "../components/SnookerScoreboardApp";
 
 export default function HomePage() {
   return (
-    <div>
-      <h1>Snooker Scoreboard</h1><br>
+    <div className="p-6">
+      <h1 className="text-4xl font-bold mb-8">Snooker Scoreboard</h1>
       <SnookerScoreboardApp />
     </div>
   );
