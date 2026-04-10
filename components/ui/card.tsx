541bdba62bef50be5050b546bf0046fa10c9efe4
phuriponga <105535944+phuriponga@users.noreply.github.com>
2026-04-10 12:51:06 +0200

    Update card.tsx

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -2,10 +2,16 @@
 
 import * as React from 'react'
 
-export function Card({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
-  return <div className={`rounded-xl border p-4 shadow ${className}`} {...props} />
+export function Card({
+  className = '',
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) {
+  return <div className={className} {...props} />
 }
 
-export function CardContent({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
-  return <div className={`p-4 ${className}`} {...props} />
+export function CardContent({
+  className = '',
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) {
+  return <div className={className} {...props} />
 }
